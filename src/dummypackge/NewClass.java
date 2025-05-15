b8ff982a1cc62287c5bc0aacbe14298981c3b3f5
sukanya-1 <sukanyabhattacharya@hotmail.com>
2025-05-15 15:06:08 -0400

    My 2nd commit

---
+++ src/dummypackge/NewClass.java
@@ -0,0 +1,29 @@
+package dummypackge;
+
+public class NewClass {
+	public class MyFirstClass {
+		
+		String name;
+		int age = 23;
+		double length = 15.253;
+		
+		static String country;
+		public static void jump() {
+			System.out.println("I Jump high");
+			
+		}
+		public static void eat() {
+			System.out.println("I eat too much");
+		}
+		public static void main (String[]args) {
+			System.out.println("My name sukanya");
+			jump();
+			eat();
+		}
+		
+
+
+	}
+
+
+}
